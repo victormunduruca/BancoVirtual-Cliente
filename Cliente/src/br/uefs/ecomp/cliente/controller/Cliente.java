8dd66d79a17ee5151f25fc54048a2907700229d9
victormunduruca <victormunduruca@gmail.com>
2017-04-13 23:11:59 -0300

    Md5 adicionado

--- Cliente/src/br/uefs/ecomp/cliente/controller/Cliente.java
+++ Cliente/src/br/uefs/ecomp/cliente/controller/Cliente.java
@@ -1,16 +1,21 @@
 package br.uefs.ecomp.cliente.controller;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 import br.uefs.ecomp.cliente.model.Acao;
 
 public class Cliente {
-	public static void main(String[] args) throws UnknownHostException, IOException {
+	public static void main(String[] args) throws UnknownHostException, IOException, NoSuchAlgorithmException {
 		Socket cliente = new Socket("127.0.0.1", 12346); // conecta com o servidor
 		System.out.println("O cliente se conectou ao servidor!");
 		
@@ -31,7 +36,7 @@ public class Cliente {
 		return acao.toString()+"-"+nome+";"+eJuridica.toString()+";"+numeroRegistro+";"+cep+";"+rua+";"+numero+";"+usuario+";"+senha; 
 	}
 	
-	public static String menu() {
+	public static String menu() throws IOException, NoSuchAlgorithmException {
 		Scanner scanner = new Scanner(System.in);
 		String pacote;
 			while(true) {
@@ -43,8 +48,8 @@ public class Cliente {
 				case 1:
 				
 					System.out.println("Digite o seu nome");
-					
-					String nome = (String) scanner.next();// Tratar pra nomes completos
+					BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+					String nome = leitor.readLine();// Tratar exception
 					boolean eJuridica = false;
 					
 					
@@ -65,23 +70,24 @@ public class Cliente {
 					System.out.println("Digite o seu CEP");
 					String cep = (String) scanner.next();
 					System.out.println("Digite a sua rua"); //tratar para ruas
-					String rua = (String) scanner.next();
+					String rua = leitor.readLine();
 					System.out.println("Digite o numero da casa");
 					String numero = (String) scanner.next();
 					
 					System.out.println("Está quase acabando, agora, escolha um usuário pra sua conta");
 					String usuario = (String) scanner.next(); 
-					System.out.println("Por último, digite uma senha para sua nova conta no Banco Virtual");
+					System.out.println("Por último, digite uma senha para sua nova conta no Banco Virtual (sem espaços)");
 					String senha = (String) scanner.next();
 					
+					String senhaMd5 = md5(senha);
 					while(true) {
 						System.out.println("Digite (1) para conta corrente e (2) para jurídica");
 						int escolha = scanner.nextInt();
 						if(escolha == 1) {
-							pacote = formataCadastroConta(Acao.CADASTRAR_CONTA_CORRENTE, nome, eJuridica, numeroRegistro, cep, rua, numero, usuario, senha);
+							pacote = formataCadastroConta(Acao.CADASTRAR_CONTA_CORRENTE, nome, eJuridica, numeroRegistro, cep, rua, numero, usuario, senhaMd5);
 							break;
 						} else if(escolha == 2) {
-							pacote = formataCadastroConta(Acao.CADASTRAR_CONTA_POUPANCA, nome, eJuridica, numeroRegistro, cep, rua, numero, usuario, senha);
+							pacote = formataCadastroConta(Acao.CADASTRAR_CONTA_POUPANCA, nome, eJuridica, numeroRegistro, cep, rua, numero, usuario, senhaMd5);
 							break;
 						}
 					}
@@ -94,5 +100,10 @@ public class Cliente {
 			}
 			
 	}
-	
+	public static String md5(String senha) throws NoSuchAlgorithmException {
+		 MessageDigest m=MessageDigest.getInstance("MD5");
+	       m.update(senha.getBytes(),0,senha.length());
+	      // System.out.println("MD5: "+new BigInteger(1,m.digest()).toString(16));
+	      return new BigInteger(1,m.digest()).toString(16);
+	}
 }
